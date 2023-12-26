f480edb0d0126a8f7788181e2a97687dfd9f9ffc
wisnadiputra1 <wisnadiputra@gmail.com>
2023-12-26 19:52:03 +0800

    search view replace

--- app/src/main/java/com/uts/mobprog210040138/MainActivity.java
+++ app/src/main/java/com/uts/mobprog210040138/MainActivity.java
@@ -10,6 +10,7 @@ import com.google.android.material.navigation.NavigationBarView;
 
 import android.view.MenuItem;
 
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
@@ -30,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Inisialisasi bottomNavigationView
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
 
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
